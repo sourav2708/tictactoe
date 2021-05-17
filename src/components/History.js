2705059248341f8bd88930e5be20904b8b8a231d
sourav2708 <souravkumar.sk30@gmail.com>
2021-05-17 10:15:25 +0530

    styled app

--- src/components/History.js
+++ src/components/History.js
@@ -2,22 +2,23 @@ import React from 'react'
 
 const History = ({ history, moveTo, currentMove }) => {
     return ( 
-        <ul>
+     <div className="history-wrapper">
+        <ul className="history">
             {history.map((_, move) => {
                  return ( 
                  <li key={move}> 
-                 <button style={ {
-                     fontWeight: move === currentMove ? 'bold' : 'normal',
-                 } }
+                 <button 
+                 className={`btn-move ${move === currentMove ? 'active' : ''}`}
                   type="button" onClick = {() => {
                    moveTo(move);
                  }}>
-                     {move===0 ? 'Go to game start' : `Go to move #${move}`}
+                     {move === 0 ? 'Go to game start' : `Go to move #${move}`}
                  </button> 
                  </li> 
                  );  
                 })}
         </ul>
+     </div>
     );
 };
 
--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -3,12 +3,31 @@ import React from 'react';
 const StatusMessage = ({winner, current}) => {
     const noMovesLeft = current.board.every(el => el !== null);
     return (
-        <h2>{winner &&  `winner is ${winner}`}
+        <div className="status-message">
+            {winner && ( 
+            <>
+            winner is{'  '}
+            <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+                {winner}
+            </span>
+            </> 
+            )}
         {!winner && 
-           !noMovesLeft && 
-           `Next Player is ${current.isXNext ? 'X' : 'O'}`}
-           {!winner && noMovesLeft && 'X and O tied' }
-        </h2>
+           !noMovesLeft && (
+           <>
+           Next Player is{' '}
+           <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+               {current.isXNext ? 'X' : 'O'}{' '}
+           </span>
+           </>
+           )}
+           {!winner && noMovesLeft && (
+             <>
+           <span className="text-green">X</span> and {' '}
+           <span className="text-orange">O</span> tied
+           </> 
+            ) }
+        </div>
     );
 };
 
